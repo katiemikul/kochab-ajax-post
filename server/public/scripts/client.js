a070001862de3638d33e3c64025ff3df09c352c4
katiemikul <katie.heding@gmail.com>
2018-04-19 11:57:53 -0500

    add a table and input data from an array

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,27 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('jQuery is loaded');
+    recordCollection();
+}
+
+function recordCollection(){
+    console.log('pulling record collect');
+    $.ajax({
+        type: 'GET',
+        url: '/recordCollection'
+    })
+    .then(function(response){
+        // for (let i=0; i<response.length; i++){
+        //     $('#recordList').append(`<li> ${response[i].title} by ${response[i].artist} total ${response[i].cost}</li>`);
+        // }
+        console.log(response);
+        response.forEach(function(record){
+            $('#recordList').append(`<tr>
+            <td>${record.title}</td>
+            <td>${record.artist}</td>
+            <td>${record.year}</td>
+            <td>${record.cost.toLocaleString('en', { style: 'currency', currency: 'USD' }).slice(0, -3)}</td>
+        </tr>`);
+        })
+    });
 }
--- server/server.js
+++ server/server.js
@@ -2,7 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-app.use(express.static('server/public'));
+//static files
+app.use(express.static('server/public')); 
+app.use(express.static('server/module'));
+
+//this will live in the database shortly
+const recordCollection=require('./modules/record-collection');
+
+//routes - get, post, put, delete requests
+app.get('/recordCollection', (req, res) => {
+    res.send(recordCollection);
+});
 
 app.listen(PORT, () => {
     console.log(`up and running on port ${PORT}`);
